ea2f78b21d29801306151985af4b8e98b1cb355e
Tianhan Lu <tianhanlu@gmail.com>
2022-10-27 22:13:10 -0600

    WIP: Allow the python clustering script to cluster with KNN

--- src/main/python/clustering.py
+++ src/main/python/clustering.py
@@ -1,4 +1,5 @@
 from sklearn.cluster import OPTICS, KMeans
+from sklearn.neighbors import NearestNeighbors
 import numpy
 import argparse
 import json
@@ -12,19 +13,39 @@ def optics(data, max_eps):
         cluster_method="xi",
         min_cluster_size=1,
     ).fit(data)
-    # print(clustering.ordering_)
-    # print(clustering.reachability_[clustering.ordering_])
+    print(f"Ordering: {clustering.ordering_}")
+    print(f"Reachability: {clustering.reachability_[clustering.ordering_]}")
+    print(f"Labels: {clustering.labels_}")
     # print(clustering.cluster_hierarchy_)
-    return clustering.labels_
+    return clustering.labels_.tolist()
 
 
-def k_means(data):
-    kmeans = KMeans(n_clusters=3, random_state=0).fit(data)
-    print(kmeans.labels_)
+# def k_means(data):
+#     kmeans = KMeans(n_clusters=3, random_state=0).fit(data)
+#     print(f"Labels: {kmeans.labels_}")
+#     return kmeans.labels_
+
+
+def knn(data):
+    neighbors = NearestNeighbors(n_neighbors=1, metric="precomputed").fit(data)
+    distances, indices = neighbors.kneighbors(data, return_distance=True)
+    print(f"Distances:\n{distances}")
+    print(f"Indices:\n{indices}")
+    print(f"Neighbors graph:\n{neighbors.kneighbors_graph(data)}")
+    print(f"Radius neighbors graph:\n{neighbors.radius_neighbors_graph(data)}")
+    # TODO: Return labels
+    return
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Parse arguments for clustering.")
+    parser.add_argument(
+        "--algorithm",
+        type=str,
+        default="optics",
+        choices=["optics", "knn"],
+        help="The output json file",
+    )
     parser.add_argument(
         "--input", type=str, required=True, help="The input json file to cluster"
     )
@@ -46,9 +67,15 @@ if __name__ == "__main__":
         print(f"Distance matrix: {distance_matrix}")
 
         data = numpy.array(distance_matrix, dtype=object)
-        labels = optics(data, max_eps=args.max_eps)
+        if args.algorithm == "optics":
+            labels = optics(data, max_eps=args.max_eps)
+        elif args.algorithm == "knn":
+            labels = knn(data)
+        else:
+            print(f"Unknown algorithm {args.algorithm}")
+            sys.exit(-1)
 
-        output_json = {"data": distance_matrix, "labels": labels.tolist()}
+        output_json = {"data": distance_matrix, "labels": labels}
         json_object = json.dumps(output_json, indent=2)
         with open(args.output, "w") as output_file:
             print(f"Write into {args.output}")
