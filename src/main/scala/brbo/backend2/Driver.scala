5dce81e648afdc045310d77e3c52d217630e56d8
Tianhan <tianhanlu@gmail.com>
2022-11-08 16:10:35 -0700

    Refactor

--- src/main/scala/brbo/backend2/Driver.scala
+++ src/main/scala/brbo/backend2/Driver.scala
@@ -10,65 +10,13 @@ import brbo.common.{CommandLineArguments, MyLogger}
 class Driver(arguments: CommandLineArguments, program: BrboProgram) {
   private val debugMode = arguments.getDebugMode
   private val logger = MyLogger.createLogger(classOf[Driver], debugMode)
-  private val SUBSTITUTION_PENALTY = 100
 
   def verify(boundAssertion: BoundAssertion): Unit = {
     val rawTraces = generateTraces()
-    val traces = clusterTraces(rawTraces)
-    traces.foreach(trace => logger.info(s"$trace"))
   }
 
   def generateTraces(): List[Interpreter.Trace] = {
     logger.info(s"Step 1: Generate traces")
     Fuzzer.fuzz(program, debugMode)
-  }
-
-  def clusterTraces(traces: List[Interpreter.Trace]): Iterable[Trace] = {
-    val STEP_2 = "Step 2: "
-    logger.info(s"${STEP_2}Cluster similar traces: ${traces.length} traces")
-    val costTraces: List[CostTrace] = traces.map(t => t.costTrace)
-
-    logger.info(s"${STEP_2}Group traces with zero distance")
-    val groupedCostTraces: Map[CostTrace, Set[CostTrace]] = TraceClustering.groupZeroDistanceTraces(costTraces)
-    logger.info(s"${STEP_2}Found ${groupedCostTraces.size} groups of traces")
-    val representativeCostTraces = groupedCostTraces.keys
-    representativeCostTraces.foreach(t => logger.traceOrError(s"\n${t.print()}"))
-
-    logger.info(s"${STEP_2}Compute a distance matrix")
-    val distanceMatrix: List[List[Int]] =
-      TraceClustering.distanceMatrix(representativeCostTraces.toList, SUBSTITUTION_PENALTY)
-
-    logger.info(s"${STEP_2}Cluster traces")
-    val traceClusteringAlgorithm: Algorithm = Optics(Some(10), metric = Precomputed)
-    val clusterLabels: List[Int] = Clustering.cluster(distanceMatrix, traceClusteringAlgorithm, debugMode) match {
-      case Some(labels) => labels
-      case None =>
-        logger.info(s"Put all traces into the same cluster")
-        List.fill(representativeCostTraces.size)(0)
-    }
-
-    val labelMap: Map[Int, List[((CostTrace, Trace), Int)]] =
-      costTraces.zip(traces).zip(clusterLabels).groupBy({ case (_, label) => label })
-    val outlierTraces = {
-      traceClusteringAlgorithm match {
-        case _: Optics =>
-          labelMap.get(-1) match {
-            case Some(outliers) =>
-              logger.info(s"${STEP_2}Found ${outliers.size} outlier traces")
-              outliers.map({ case ((_, trace), _) => trace })
-            case None => Nil
-          }
-        case _ => Nil
-      }
-    }
-    val clusters: Iterable[List[((CostTrace, Trace), Int)]] = (labelMap - (-1)).values
-    logger.info(s"${STEP_2}Found ${clusters.size} trace clusters")
-
-    logger.info(s"${STEP_2}Select representative traces from every cluster")
-    clusters.map({
-      list =>
-        val traces = list.map({ case ((_, trace), _) => trace })
-        TraceClustering.selectRepresentativeTrace(traces)
-    }).toList ::: outlierTraces
   }
 }
--- src/main/scala/brbo/backend2/learning/TraceClustering.scala
+++ src/main/scala/brbo/backend2/learning/TraceClustering.scala
@@ -2,9 +2,62 @@ package brbo.backend2.learning
 
 import brbo.backend2.interpreter.Interpreter
 import brbo.backend2.interpreter.Interpreter.{CostTrace, Trace}
-import brbo.common.DisjointSet
+import brbo.backend2.learning.ScriptRunner.{Algorithm, Optics, Precomputed}
+import brbo.common.{DisjointSet, MyLogger}
 
 object TraceClustering {
+  private val logger = MyLogger.createLogger(TraceClustering.getClass, debugMode = false)
+  private val SUBSTITUTION_PENALTY = 100
+
+  def run(traces: List[Interpreter.Trace], debugMode: Boolean): Iterable[Trace] = {
+    val STEP_NAME = "Step 2: "
+    logger.info(s"${STEP_NAME}Cluster similar traces: ${traces.length} traces")
+    val costTraces: List[CostTrace] = traces.map(t => t.costTrace)
+
+    logger.info(s"${STEP_NAME}Group traces with zero distance")
+    val groupedCostTraces: Map[CostTrace, Set[CostTrace]] = TraceClustering.groupZeroDistanceTraces(costTraces)
+    logger.info(s"${STEP_NAME}Found ${groupedCostTraces.size} groups of traces")
+    val representativeCostTraces = groupedCostTraces.keys
+    representativeCostTraces.foreach(t => logger.traceOrError(s"\n${t.print()}"))
+
+    logger.info(s"${STEP_NAME}Compute a distance matrix")
+    val distanceMatrix: List[List[Int]] =
+      TraceClustering.distanceMatrix(representativeCostTraces.toList, SUBSTITUTION_PENALTY)
+
+    logger.info(s"${STEP_NAME}Cluster traces")
+    val traceClusteringAlgorithm: Algorithm = Optics(Some(10), metric = Precomputed)
+    val clusterLabels: List[Int] = Clustering.cluster(distanceMatrix, traceClusteringAlgorithm, debugMode) match {
+      case Some(labels) => labels
+      case None =>
+        logger.info(s"Put all traces into the same cluster")
+        List.fill(representativeCostTraces.size)(0)
+    }
+
+    val labelMap: Map[Int, List[((CostTrace, Trace), Int)]] =
+      costTraces.zip(traces).zip(clusterLabels).groupBy({ case (_, label) => label })
+    val outlierTraces = {
+      traceClusteringAlgorithm match {
+        case _: Optics =>
+          labelMap.get(-1) match {
+            case Some(outliers) =>
+              logger.info(s"${STEP_NAME}Found ${outliers.size} outlier traces")
+              outliers.map({ case ((_, trace), _) => trace })
+            case None => Nil
+          }
+        case _ => Nil
+      }
+    }
+    val clusters: Iterable[List[((CostTrace, Trace), Int)]] = (labelMap - (-1)).values
+    logger.info(s"${STEP_NAME}Found ${clusters.size} trace clusters")
+
+    logger.info(s"${STEP_NAME}Select representative traces from every cluster")
+    clusters.map({
+      list =>
+        val traces = list.map({ case ((_, trace), _) => trace })
+        TraceClustering.selectRepresentativeTrace(traces)
+    }).toList ::: outlierTraces
+  }
+
   def groupZeroDistanceTraces(traces: List[CostTrace]): Map[CostTrace, Set[CostTrace]] = {
     val disjointSet = new DisjointSet[CostTrace](
       (left, right) => distance(left, right, substitutionPenalty = 1) == 0,
