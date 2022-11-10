942d2eff0edcc4299b15e4b7997ec3825ddd7105
Tianhan <tianhanlu@gmail.com>
2022-11-09 19:59:31 -0700

    Add one more benchmark

---
+++ src/main/java/brbo/benchmarks/sas22/limitation/SubsequenceDifferentDecomposition.java
@@ -0,0 +1,20 @@
+package brbo.benchmarks.sas22.limitation;
+
+import brbo.benchmarks.Common;
+
+public abstract class SubsequenceDifferentDecomposition extends Common {
+    void main(int costA, int costB, int costC, int n) {
+        upperBound(0, "most", (costA >= costB ? costA : costB) + 2 * costA);
+        int e = costB;
+        if (n > 0)
+            e = costA;
+        use(0, e);
+
+        e = costC;
+        if (n > 0)
+            e = costA;
+        use(0, e);
+        // Although path with costs B, C is a subsequence of path with costs A, A,
+        // the two paths need to be decomposed differently
+    }
+}
