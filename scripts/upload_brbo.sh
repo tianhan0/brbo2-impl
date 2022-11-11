27a470bd24b6dc958ff645f5f38662860787e13c
Tianhan <tianhanlu@gmail.com>
2022-11-10 19:14:55 -0700

    Add scripts for uploading artifacts to the server

---
+++ scripts/upload_brbo.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+rsync -r $HOME/Desktop/brbo-impl.jar root@164.92.214.247:/root/Desktop/
---
+++ scripts/upload_results.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+rsync -r output/ root@164.92.214.247:/root/Documents/workspace/brbo-impl/output/
