3df96423166efd2df0b2501b09fe9de07c684037
Tianhan <tianhanlu@gmail.com>
2022-12-11 10:46:02 -0700

    Rewrite stac/TemplateEngine.java with array operations

--- src/main/java/brbo/benchmarks/sas22/sas21/stac/TemplateEngine.java
+++ src/main/java/brbo/benchmarks/sas22/sas21/stac/TemplateEngine.java
@@ -3,28 +3,25 @@ package brbo.benchmarks.sas22.sas21.stac;
 import brbo.benchmarks.Common;
 
 public abstract class TemplateEngine extends Common {
-  void main(int text) {
-    if (text <= 0) {
+  void main(int[] text) {
+    if (arraySum(text) <= 0) {
       return;
     }
     int R = 0;
-    mostPreciseBound(R <= text);
-    lessPreciseBound(R <= MAX * text + MAX);
+    mostPreciseBound(R <= arraySum(text));
+    lessPreciseBound(R <= MAX * arraySum(text) + MAX);
 
-    int linePointer = 0;
-    int startTagLocation = 0;
-    int endTagLocation = 0;
-    int stringBuilder = 0;
-    while (endTagLocation < text) {
-      startTagLocation = ndInt2(endTagLocation + 1, text);
-      if (startTagLocation + 1 > text) break;
-      endTagLocation = ndInt2(startTagLocation + 1, text);
-      stringBuilder += startTagLocation - linePointer;
-      R = R + (startTagLocation - linePointer);
-      linePointer = endTagLocation;
+    int chunk = 0;
+    int tag = 0;
+    int i = 0;
+    chunk = arrayRead(text, i);
+    R = R + chunk;
+    i++;
+    while (i + 1 < arrayLength(text)) {
+      tag = arrayRead(text, i);
+      chunk = arrayRead(text, i + 1);
+      R = R + chunk;
+      i++;
     }
-    stringBuilder += text - linePointer;
-    R = R + (text - linePointer);
   }
-  // No (Imprecise); Yes; Yes (Same as Brbo)
 }
