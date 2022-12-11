880a36968475db742e313942538aa46182e88e74
Tianhan <tianhanlu@gmail.com>
2022-12-11 11:10:31 -0700

    Rewrite stac/TemplateEngine2.java and stac/TemplateEngine3.java

--- src/main/java/brbo/benchmarks/sas22/sas21/stac/TemplateEngine3.java
+++ src/main/java/brbo/benchmarks/sas22/sas21/stac/TemplateEngine3.java
@@ -3,23 +3,23 @@ package brbo.benchmarks.sas22.sas21.stac;
 import brbo.benchmarks.Common;
 
 public abstract class TemplateEngine3 extends Common {
-  void main(int text, int ts, int sep, int tags, int rep) {
-    if (text <= 0 || ts <= 0 || sep <= 0 || tags <= 0 || rep <= 0) {
+  void main(int[] text, int ts, int sep, int tags, int rep) {
+    if (arraySum(text) <= 0 || ts <= 0 || sep <= 0 || tags <= 0 || rep <= 0) {
       return;
     }
     int R = 0;
-    mostPreciseBound(R <= ts * (text + tags * rep + sep));
-    lessPreciseBound(R <= MAX * text * ts +
-        MAX * text * sep +
-        MAX * text * tags +
-        MAX * text * rep +
+    mostPreciseBound(R <= ts * (arraySum(text) + tags * rep + sep));
+    lessPreciseBound(R <= MAX * arraySum(text) * ts +
+        MAX * arraySum(text) * sep +
+        MAX * arraySum(text) * tags +
+        MAX * arraySum(text) * rep +
         MAX * ts * sep +
         MAX * ts * tags +
         MAX * ts * rep +
         MAX * sep * tags +
         MAX * sep * rep +
         MAX * tags * rep+
-        MAX * text +
+        MAX * arraySum(text) +
         MAX * ts +
         MAX * sep +
         MAX * tags +
@@ -28,25 +28,23 @@ public abstract class TemplateEngine3 extends Common {
     );
 
     int i = 0;
+    int j = 0;
+    int chunk = 0;
+    int tag = 0;
     while (i < ts) {
-      int p = 0;
-      int l = 0;
-      int r = 0;
-      int stringBuilder = 0;
-      while (r <= text) {
-        l = ndInt2(p, text);
-        if (l + 1 > text) break;
-        r = ndInt2(l + 1, text);
-        stringBuilder += l - p;
-        R = R + (l - p);
-        stringBuilder += rep;
+      j = 0;
+      chunk = arrayRead(text, j);
+      R = R + chunk;
+      j++;
+      while (j + 1 < arrayLength(text)) {
+        tag = arrayRead(text, j);
+        chunk = arrayRead(text, j + 1);
+        R = R + chunk;
         R = R + rep;
-        p = r;
+        j++;
       }
-      stringBuilder += text - p;
-      R = R + (text - p);
-      R = R + sep;
       i++;
+      R = R + sep;
     }
   }
 }
