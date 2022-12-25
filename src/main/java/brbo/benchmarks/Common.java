425a4a62c15291ee13f98b3146f982c56f05ceaf
Tianhan <tianhanlu@gmail.com>
2022-12-24 18:54:23 -0800

    Update Common.java

--- src/main/java/brbo/benchmarks/Common.java
+++ src/main/java/brbo/benchmarks/Common.java
@@ -1,45 +1,34 @@
 package brbo.benchmarks;
 
 public abstract class Common {
-    public int MAX = 8;
-    public int LARGE_INT = 10000000;
+  public int MAX = 8;
+  public int LARGE_INT = 10000000;
 
-    public abstract int arrayRead(int[] x, int index);
+  public abstract int arrayRead(int[] x, int index);
 
-    public abstract int arrayLength(int[] x);
+  public abstract int arrayLength(int[] x);
 
-    public abstract int arraySum(int[] x);
+  public abstract int arraySum(int[] x);
 
-    public abstract void use(int group, int cost, boolean condition);
+  public abstract void use(int group, int cost, boolean condition);
 
-    public abstract void use(int group, int cost);
+  public abstract void use(int group, int cost);
 
-    public abstract void reset(int group, boolean condition);
+  public abstract void reset(int group, boolean condition);
 
-    public abstract void reset(int x);
+  public abstract void reset(int x);
 
-    /**
-     * @return Non-deterministic integer
-     */
-    public abstract int ndInt();
+  public abstract int ndInt(); // Non-deterministic integer
 
-    /**
-     * @param lower
-     * @param upper
-     * @return Return a non-deterministic integer in [lower, upper]
-     */
-    public abstract int ndInt2(int lower, int upper);
+  public abstract int ndInt2(int lower, int upper); //  Return a non-deterministic integer in [lower, upper]
 
-    /**
-     * @return Non-deterministic boolean
-     */
-    public abstract boolean ndBool();
+  public abstract boolean ndBool(); // Non-deterministic boolean
 
-    public abstract void assume(boolean expression);
+  public abstract void assume(boolean expression);
 
-    public abstract void mostPreciseBound(boolean assertion);
+  public abstract void mostPreciseBound(boolean assertion);
 
-    public abstract void lessPreciseBound(boolean assertion);
+  public abstract void lessPreciseBound(boolean assertion);
 
-    public abstract void resetPlaceHolder();
+  public abstract void resetPlaceHolder();
 }
