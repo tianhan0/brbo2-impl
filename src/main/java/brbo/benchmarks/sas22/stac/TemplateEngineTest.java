a015cd8fd9a57fc173168a5af6747aa1a5db86cc
Tianhan <tianhanlu@gmail.com>
2022-12-27 19:32:48 -0800

    Add QFuzz outputs for stac

---
+++ src/main/java/brbo/benchmarks/sas22/stac/TemplateEngineTest.java
@@ -0,0 +1,26 @@
+package brbo.benchmarks.sas22.stac;
+
+import brbo.benchmarks.Common;
+
+public abstract class TemplateEngineTest extends Common {
+  void execute(int[] text) {
+    if (arraySum(text) <= 0) {
+      return;
+    }
+    int R = 0;
+    mostPreciseBound(R <= arraySum(text));
+    lessPreciseBound(R <= MAX * arraySum(text) + MAX);
+
+    int chunk = 0;
+    int tag = 0;
+    int i = 0;
+    chunk = arrayRead(text, i);
+    R = R + chunk;
+    i++;
+    while (i < arrayLength(text)) {
+      chunk = arrayRead(text, i);
+      R = R + chunk;
+      i += 1;
+    }
+  }
+}
