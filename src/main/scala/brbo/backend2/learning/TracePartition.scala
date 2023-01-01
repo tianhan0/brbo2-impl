aa4ce03cefc573ef5e5549cb674db85829e2955a
Tianhan <tianhanlu@gmail.com>
2023-01-01 11:42:30 -0800

    Do not sort the QFuzz-generated traces before selecting representatives

--- src/main/scala/brbo/backend2/learning/TracePartition.scala
+++ src/main/scala/brbo/backend2/learning/TracePartition.scala
@@ -18,30 +18,35 @@ object TracePartition {
         logger.info(s"There is no fuzzer generated traces. Exiting")
         sys.exit(-1)
       }
-      return select(fuzzerGeneratedTraces)
+      return select(fuzzerGeneratedTraces, sortByLengths = true)
     }
     if (userProvidedTraces.nonEmpty) {
       logger.info(s"$prefix Select representatives from user-provided traces")
-      return select(userProvidedTraces).map({
+      return select(userProvidedTraces, sortByLengths = true).map({
         case (trace, similarTraces) =>
           // Ensure the fuzzer generated traces will be used for testing generality
           (trace, similarTraces ::: fuzzerGeneratedTraces ::: qfuzzGeneratedTraces)
       })
     }
     logger.info(s"$prefix Select representatives from qfuzz generated traces")
-    select(qfuzzGeneratedTraces).map({
+    select(qfuzzGeneratedTraces, sortByLengths = false).map({
       case (trace, similarTraces) =>
         // Ensure the fuzzer generated traces will be used for testing generality
         (trace, similarTraces ::: fuzzerGeneratedTraces)
     })
   }
 
-  private def select(traces: List[Trace]): Map[Trace, List[Trace]] = {
-    val sorted = traces.sortWith({
-      case (trace1, trace2) => trace1.nodes.length < trace2.nodes.length
-    })
-    // It is best to choose a trace that starts to show patterns (compared with shorter traces, such that
-    // the pattern is generalizable), but is not too long (to avoid over-fitting)
+  private def select(traces: List[Trace], sortByLengths: Boolean): Map[Trace, List[Trace]] = {
+    val sorted = {
+      if (sortByLengths) {
+        // It is best to choose a trace that starts to show patterns (compared with shorter traces, such that
+        // the pattern is generalizable), but is not too long (to avoid over-fitting)
+        traces.sortWith({
+          case (trace1, trace2) => trace1.nodes.length < trace2.nodes.length
+        })
+      } else
+        traces
+    }
     val chosenIndex = (sorted.length * 0.0).toInt
     val indexRange = Range.inclusive(chosenIndex, chosenIndex + numberOfTraces - 1).intersect(sorted.indices)
     logger.info(s"Choose traces within range $indexRange from ${sorted.length} traces")
