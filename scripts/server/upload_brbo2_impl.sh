e1f1aa22275adbed8faa29b338f25baecfeb2e33
Tianhan <tianhanlu@gmail.com>
2023-01-04 18:40:41 -0700

    Update upload_brbo2_impl.sh: Do not delete the files that exist on server but not on mac

--- scripts/server/upload_brbo2_impl.sh
+++ scripts/server/upload_brbo2_impl.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-rsync --archive --compress --verbose --progress --delete --force \
+rsync --archive --compress --verbose --progress \
   --human-readable --exclude target/ \
   --recursive $HOME/Documents/workspace/brbo2-impl/ root@134.209.201.43:/root/Documents/workspace/brbo2-impl/
