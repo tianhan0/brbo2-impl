61f66723229582dcf5bdba72a8b61710c0682e81
Tianhan <tianhanlu@gmail.com>
2023-01-05 19:04:49 -0700

    Update TemplateEngine3.java
    This is the version that ICRA is able to infer tight bounds on variables, with a timeout of 150s.

--- output/decomposed/stac/TemplateEngine3.java
+++ output/decomposed/stac/TemplateEngine3.java
@@ -1,7 +1,7 @@
 abstract class TemplateEngine3 {
-  void main(int text, int ts, int sep, int rep) 
-
+  void execute(int text, int ts, int sep, int rep) 
   {
+    int BOOLEAN_SEPARATOR = 500;
     int C0 = -1;
     int C1 = -1;
     int C2 = -1;
@@ -11,15 +11,17 @@ abstract class TemplateEngine3 {
     int D1p = 0;
     int D2 = 0;
     int D2p = 0;
-    lessPreciseBound(((((0 + D0) + D1) + D2) < (((((((((((((((((8 * arraySum(text)) * ts) + ((8 * arraySum(text)) * sep)) + ((8 * arraySum(text)) * arraySum(text))) + ((8 * arraySum(text)) * rep)) + ((8 * ts) * sep)) + ((8 * ts) * arraySum(text))) + ((8 * ts) * rep)) + ((8 * sep) * arraySum(text))) + ((8 * sep) * rep)) + ((8 * arraySum(text)) * rep)) + (8 * arraySum(text))) + (8 * ts)) + (8 * sep)) + (8 * arraySum(text))) + (8 * rep)) + 8)) || ((((0 + D0) + D1) + D2) == (((((((((((((((((8 * arraySum(text)) * ts) + ((8 * arraySum(text)) * sep)) + ((8 * arraySum(text)) * arraySum(text))) + ((8 * arraySum(text)) * rep)) + ((8 * ts) * sep)) + ((8 * ts) * arraySum(text))) + ((8 * ts) * rep)) + ((8 * sep) * arraySum(text))) + ((8 * sep) * rep)) + ((8 * arraySum(text)) * rep)) + (8 * arraySum(text))) + (8 * ts)) + (8 * sep)) + (8 * arraySum(text))) + (8 * rep)) + 8)));
-    mostPreciseBound(((((0 + D0) + D1) + D2) < (ts * ((arraySum(text) + (arraySum(text) * rep)) + sep))) || ((((0 + D0) + D1) + D2) == (ts * ((arraySum(text) + (arraySum(text) * rep)) + sep))));
-    if (((((arraySum(text) < 0) || (arraySum(text) == 0)) || ((ts < 0) || (ts == 0))) || ((sep < 0) || (sep == 0))) || ((rep < 0) || (rep == 0)))
+    int temporaryText = 0;
+    int lastIndexOfText = 0;
+    lessPreciseBound(((((0 + D0) + D1) + D2) < (((((((((((((((((8 * text) * ts) + ((8 * text) * sep)) + ((8 * text) * text)) + ((8 * text) * rep)) + ((8 * ts) * sep)) + ((8 * ts) * text)) + ((8 * ts) * rep)) + ((8 * sep) * text)) + ((8 * sep) * rep)) + ((8 * text) * rep)) + (8 * text)) + (8 * ts)) + (8 * sep)) + (8 * text)) + (8 * rep)) + 8)) || ((((0 + D0) + D1) + D2) == (((((((((((((((((8 * text) * ts) + ((8 * text) * sep)) + ((8 * text) * text)) + ((8 * text) * rep)) + ((8 * ts) * sep)) + ((8 * ts) * text)) + ((8 * ts) * rep)) + ((8 * sep) * text)) + ((8 * sep) * rep)) + ((8 * text) * rep)) + (8 * text)) + (8 * ts)) + (8 * sep)) + (8 * text)) + (8 * rep)) + 8)));
+    mostPreciseBound(((((0 + D0) + D1) + D2) < (ts * ((text + (text * rep)) + sep))) || ((((0 + D0) + D1) + D2) == (ts * ((text + (text * rep)) + sep))));
+    if (((((text < 0) || (text == 0)) || ((ts < 0) || (ts == 0))) || ((sep < 0) || (sep == 0))) || ((rep < 0) || (rep == 0)))
     {
       return;
     }
     else
     {
-
+      ;
     }
     int R = 0;
     // mostPreciseBound(R <= ts * (arraySum(text) + arraySum(text) * rep + sep))
@@ -30,68 +32,51 @@ abstract class TemplateEngine3 {
     int tag = 0;
     while (i < ts)
     {
+      j = 0;
       {
-        j = 0;
-        chunk = arrayRead(text, j);
+        temporaryText = ndInt2(lastIndexOfText, text);
+        chunk = temporaryText - lastIndexOfText;
+        lastIndexOfText = temporaryText;
+      }
+      D2 = D2 + chunk;
+      j = j + 1;
+      while (j < (text))
+      {
+        {
+          temporaryText = ndInt2(lastIndexOfText, text);
+          chunk = temporaryText - lastIndexOfText;
+          lastIndexOfText = temporaryText;
+        }
+        {
+          temporaryText = ndInt2(lastIndexOfText, text);
+          tag = temporaryText - lastIndexOfText;
+          lastIndexOfText = temporaryText;
+        }
         D2 = D2 + chunk;
+        D0 = D0 + rep;
         j = j + 1;
-        while ((j + 1) < arrayLength(text))
-        {
-          {
-            tag = arrayRead(text, j);
-            chunk = arrayRead(text, (j + 1));
-            D2 = D2 + chunk;
-            D0 = D0 + rep;
-            j = j + 2;
-          }
-          {
-            // reset R2
-            // reset R1
-            if (D0p < D0)
-            {
-              D0p = D0;
-            }
-            else
-            {
-              ;
-            }
-            D0 = 0;
-            C0 = C0 + 1;
-          }
-        }
-        i = i + 1;
-        D1 = D1 + sep;
+        // reset R2
+        // reset R1
+        D0p = D0;
+        D0 = 0;
+        C0 = C0 + 1;
       }
-      {
-        if (D2p < D2)
-        {
-          D2p = D2;
-        }
-        else
-        {
-          ;
-        }
-        D2 = 0;
-        C2 = C2 + 1;
-        if (D1p < D1)
-        {
-          D1p = D1;
-        }
-        else
-        {
-          ;
-        }
-        D1 = 0;
-        C1 = C1 + 1;
-        // reset R0
-      }
+      i = i + 1;
+      D1 = D1 + sep;
+      D2p = D2;
+      D2 = 0;
+      C2 = C2 + 1;
+      D1p = D1;
+      D1 = 0;
+      C1 = C1 + 1;
+      // reset R0
     }
   }
+  // Declare these methods such that these generated code can be parsed
   public abstract int ndInt();
   public abstract int ndInt2(int lower, int upper);
   public abstract boolean ndBool();
   public abstract void assume(boolean expression);
   public abstract void mostPreciseBound(boolean assertion);
   public abstract void lessPreciseBound(boolean assertion);
-  public abstract void resetPlaceHolder();
 }
