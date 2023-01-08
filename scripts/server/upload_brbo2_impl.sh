0a44fd6ae3438f5390906c21b78ee2c4e9fb5285
Tianhan <tianhanlu@gmail.com>
2023-01-07 19:09:44 -0700

    Update upload_brbo2_impl.sh

--- scripts/server/upload_brbo2_impl.sh
+++ scripts/server/upload_brbo2_impl.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-rsync --archive --compress --verbose --progress --executability \
+rsync --archive --compress --verbose --progress --executability --delete \
   --human-readable --exclude target/ \
   --recursive $HOME/Documents/workspace/brbo2-impl/ root@134.209.201.43:/root/Documents/workspace/brbo2-impl/
