9e37675ba7220ec7f2ae10ab2cc616ec932992b7
Tianhan <tianhanlu@gmail.com>
2023-01-09 20:01:29 -0700

    Update upload_brbo2_impl.sh

--- scripts/server/upload_brbo2_impl.sh
+++ scripts/server/upload_brbo2_impl.sh
@@ -2,5 +2,5 @@
 
 rsync --archive --compress --verbose --progress --executability --delete \
   --human-readable \
-  --exclude target/ --exclude output/ \
+  --exclude target/ --exclude output/ --exclude src/main/java/brbo/fuzz/drivers/ \
   --recursive $HOME/Documents/workspace/brbo2-impl/ root@134.209.201.43:/root/Documents/workspace/brbo2-impl/
