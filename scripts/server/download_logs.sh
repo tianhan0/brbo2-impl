273cc8934913ef42ac4a8a16941b5d04618f4c77
Tianhan <tianhanlu@gmail.com>
2023-01-10 22:54:16 -0700

    Delete download_logs.sh

--- scripts/server/download_logs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-rsync --archive --compress --verbose --progress --executability \
-  --human-readable \
-  root@134.209.201.43:/root/Documents/workspace/log*.txt $HOME/Documents/workspace/brbo2-impl/output/logs/
