d9d6d9cc722268a2dac95ff3cd65a8b6b78c90c3
Tianhan <tianhanlu@gmail.com>
2023-01-10 22:54:30 -0700

    Update download_brbo2_impl.sh

--- scripts/server/download_brbo2_impl.sh
+++ scripts/server/download_brbo2_impl.sh
@@ -2,5 +2,5 @@
 
 rsync --archive --compress --verbose --progress --executability --delete \
   --human-readable \
-  --exclude target/ --exclude output/cfg/ \
+  --exclude target/ --exclude output/cfg/ --exclude output/fuzz/ --exclude output/cmd/ --exclude src/main/java/brbo/fuzz/drivers/ \
   --recursive root@134.209.201.43:/root/Documents/workspace/brbo2-impl/ $HOME/Documents/workspace/brbo2-impl/
