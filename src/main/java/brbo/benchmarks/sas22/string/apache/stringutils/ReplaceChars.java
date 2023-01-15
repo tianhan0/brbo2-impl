b45d7912be01468bb4c46767db52bc7a39015b3c
Tianhan <tianhanlu@gmail.com>
2023-01-15 16:39:45 -0700

    Update ReplaceChars.java

--- src/main/java/brbo/benchmarks/sas22/string/apache/stringutils/ReplaceChars.java
+++ src/main/java/brbo/benchmarks/sas22/string/apache/stringutils/ReplaceChars.java
@@ -3,15 +3,16 @@ package brbo.benchmarks.sas22.string.apache.stringutils;
 import brbo.benchmarks.Common;
 
 abstract public class ReplaceChars extends Common {
-  void execute(int str, int choice, int searchChars, int replaceChars) {
-    if (str <= 0 || searchChars <= 0 || replaceChars <= 0 || !(choice >= 0 && choice < str)) {
+  void execute(int str, int replaceChars) {
+    if (str <= 0 || replaceChars <= 0) {
       return;
     }
     int R = 0;
     mostPreciseBound(R <= str);
     lessPreciseBound(R <= MAX * str + MAX);
+    int choice = ndInt2(0, str - 1);
     for (int i = 0; i < str; i++) {
-      if (i > choice) {
+      if (i >= choice) {
         // Replace
         if (i < replaceChars) {
           R = R + 1;
