b882d41e2148c475bfc2807b1ab3ef345e5a80ed
Tianhan <tianhanlu@gmail.com>
2023-01-15 16:07:06 -0700

    Update AppendQuotedString.java

--- src/main/java/brbo/benchmarks/sas22/string/apache/lang3/AppendQuotedString.java
+++ src/main/java/brbo/benchmarks/sas22/string/apache/lang3/AppendQuotedString.java
@@ -3,18 +3,21 @@ package brbo.benchmarks.sas22.string.apache.lang3;
 import brbo.benchmarks.Common;
 
 abstract public class AppendQuotedString extends Common {
-  void execute(int pattern, int pos, int stopIndex) {
-    if (pattern <= 0 || pos <= 0 || pattern < pos || !(stopIndex <= pattern && stopIndex >= pos)) {
+  void execute(int pattern, int[] stopIndex) {
+    if (pattern <= 0 || arrayLength(stopIndex) <= pattern) {
       return;
     }
     int appendTo = 0;
     int R = 0;
     mostPreciseBound(R <= pattern);
-    lessPreciseBound(R <= MAX * pattern + MAX * pos + MAX);
+    lessPreciseBound(R <= MAX * pattern + MAX);
+    int pos = ndInt2(0, pattern);
     int start = pos;
     int lastHold = start;
+    int choice = 0;
     for (int i = pos; i < pattern; i++) {
-      if (i == stopIndex) {
+      choice = arrayRead(stopIndex, i);
+      if (choice <= BOOLEAN_SEPARATOR) {
         pos++;
         appendTo += pos - lastHold;
         R = R + (pos - lastHold);
