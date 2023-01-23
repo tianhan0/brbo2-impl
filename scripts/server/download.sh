87798c9696ab9b54d992a711debf701658e00528
Tianhan <tianhanlu@gmail.com>
2023-01-23 15:14:25 -0800

    Update server scripts

--- scripts/server/download.sh
+++ scripts/server/download.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
-rsync --archive --compress --verbose --progress --executability --delete \
+rsync --archive --compress --verbose --progress --executability \
   --human-readable \
-  --exclude target/ --exclude output/cfg/ --exclude output/ \
-  --exclude src/main/java/brbo/fuzz/drivers/ --exclude .git/ \
+  --exclude target/ \
+  --exclude output/ \
+  --exclude src/main/java/brbo/fuzz/drivers/ \
+  --exclude .git/ \
   --recursive root@134.209.201.43:/root/Documents/workspace/brbo2-impl/ $HOME/Documents/workspace/brbo2-impl/
 
-rsync --archive --compress --verbose --progress --executability --delete \
+rsync --archive --compress --verbose --progress --executability \
   --human-readable \
-  --exclude target/ --exclude output/cfg/ --exclude output/ \
-  --exclude src/main/java/brbo/fuzz/drivers/ --exclude .git/ \
-  --recursive root@159.89.85.242:/root/Documents/workspace/brbo2-impl/ $HOME/Documents/workspace/brbo2-impl/
+  --exclude target/ \
+  --exclude output/ \
+  --exclude src/main/java/brbo/fuzz/drivers/ \
+  --exclude .git/ \
+  --recursive root@64.227.67.224:/root/Documents/workspace/brbo2-impl/ $HOME/Documents/workspace/brbo2-impl/
--- scripts/server/upload.sh
+++ scripts/server/upload.sh
@@ -2,10 +2,14 @@
 
 rsync --archive --compress --verbose --progress --executability --delete \
   --human-readable \
-  --exclude target/ --exclude output/ --exclude src/main/java/brbo/fuzz/drivers/ \
+  --exclude target/ \
+  --exclude output/ \
+  --exclude src/main/java/brbo/fuzz/drivers/ \
   --recursive $HOME/Documents/workspace/brbo2-impl/ root@134.209.201.43:/root/Documents/workspace/brbo2-impl/
 
 rsync --archive --compress --verbose --progress --executability --delete \
   --human-readable \
-  --exclude target/ --exclude output/ --exclude src/main/java/brbo/fuzz/drivers/ \
-  --recursive $HOME/Documents/workspace/brbo2-impl/ root@159.89.85.242:/root/Documents/workspace/brbo2-impl/
+  --exclude target/ \
+  --exclude output/ \
+  --exclude src/main/java/brbo/fuzz/drivers/ \
+  --recursive $HOME/Documents/workspace/brbo2-impl/ root@64.227.67.224:/root/Documents/workspace/brbo2-impl/
