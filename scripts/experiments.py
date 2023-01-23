76e5ac9ed334e5508716f7b6cfaa1565b14d4b70
Tianhan <tianhanlu@gmail.com>
2023-01-22 21:51:19 -0800

    Enable running the comparison between naive and modified qfuzz via a script

--- scripts/experiments.py
+++ scripts/experiments.py
@@ -1,10 +1,12 @@
 import argparse
 import logging
+import os
 from pathlib import Path
 from common import run_command, print_args
+from datetime import datetime
 
 
-def brbo2_command(input, qfuzz, brbo, icra, dry, timeout, log_file):
+def brbo2_command(input, qfuzz, brbo, icra, dry, timeout, log_file, mode):
     brbo2_command = f"""python3 scripts/brbo2.py \
       --input {input} \
       --qfuzz {qfuzz} \
@@ -12,6 +14,7 @@ def brbo2_command(input, qfuzz, brbo, icra, dry, timeout, log_file):
       --icra {icra} \
       --timeout {timeout} \
       --log {log_file} \
+      --mode {mode} \
       {"--dry" if dry else ""}"""
     return brbo2_command.split()
 
@@ -81,8 +84,12 @@ if __name__ == "__main__":
     print_args(args)
 
     icra_timeout_in_seconds = 60
+    log_directory = Path(os.getcwd()) / "logs"
+    qfuzz_log_directory = log_directory / "qfuzz"
+    current_date_time = datetime.now().strftime("%Y%m%d-%H_%M_%S")
+
     for i in range(args.repeat):
-        logging.info(f"{i} run")
+        logging.info(f"Begin {i} run")
         if args.experiment == "verifiability" or args.experiment == "all":
             selective_amortization = brbo2_command(
                 input=args.input,
@@ -92,8 +99,9 @@ if __name__ == "__main__":
                 dry=args.dry,
                 timeout=60,
                 log_file="a.json",
+                mode="qfuzz",
             )
-            run_command(command=selective_amortization)
+            run_command(command=selective_amortization, dry=args.dry)
 
             worst_case = brbo_command(
                 input=args.input,
@@ -114,8 +122,34 @@ if __name__ == "__main__":
                 log_file="a.json",
             )
         elif args.experiment == "qfuzz" or args.experiment == "all":
-            unmodified = []
-            modified = []
+            timeout = 180
+            current_log_directory = qfuzz_log_directory / current_date_time
+            current_log_directory.mkdir(parents=True, exist_ok=True)
+            run_id = "{:03d}".format(i)
+
+            naive_qfuzz = brbo2_command(
+                input=args.input,
+                qfuzz=args.qfuzz,
+                brbo=args.brbo,
+                icra=args.icra,
+                dry=args.dry,
+                timeout=timeout,
+                log_file=current_log_directory / f"naive_{run_id}.json",
+                mode="naive",
+            )
+            run_command(command=naive_qfuzz, dry=args.dry)
+
+            modified_qfuzz = brbo2_command(
+                input=args.input,
+                qfuzz=args.qfuzz,
+                brbo=args.brbo,
+                icra=args.icra,
+                dry=args.dry,
+                timeout=timeout,
+                log_file=current_log_directory / f"qfuzz_{run_id}.json",
+                mode="qfuzz",
+            )
+            run_command(command=modified_qfuzz, dry=args.dry)
         elif args.experiment == "timeout" or args.experiment == "all":
             timeout_30 = brbo2_command(
                 input=args.input,
@@ -125,4 +159,5 @@ if __name__ == "__main__":
                 dry=args.dry,
                 timeout=30,
                 log_file="a.json",
+                mode="qfuzz",
             )
