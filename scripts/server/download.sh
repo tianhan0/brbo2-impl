35a57be779895116b788b0c0b4c7a772f39677ee
Tianhan <tianhanlu@gmail.com>
2023-01-23 14:59:23 -0800

    Add new server IP address

--- scripts/server/download.sh
+++ scripts/server/download.sh
@@ -5,3 +5,9 @@ rsync --archive --compress --verbose --progress --executability --delete \
   --exclude target/ --exclude output/cfg/ --exclude output/ \
   --exclude src/main/java/brbo/fuzz/drivers/ --exclude .git/ \
   --recursive root@134.209.201.43:/root/Documents/workspace/brbo2-impl/ $HOME/Documents/workspace/brbo2-impl/
+
+rsync --archive --compress --verbose --progress --executability --delete \
+  --human-readable \
+  --exclude target/ --exclude output/cfg/ --exclude output/ \
+  --exclude src/main/java/brbo/fuzz/drivers/ --exclude .git/ \
+  --recursive root@159.89.85.242:/root/Documents/workspace/brbo2-impl/ $HOME/Documents/workspace/brbo2-impl/
