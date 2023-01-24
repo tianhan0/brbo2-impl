17379531721e9451a5c9dda24bfe61de5dc7ea1c
Tianhan <tianhanlu@gmail.com>
2023-01-23 16:14:33 -0800

    Update upload.sh

--- scripts/server/upload.sh
+++ scripts/server/upload.sh
@@ -4,6 +4,7 @@ rsync --archive --compress --verbose --progress --executability --delete \
   --human-readable \
   --exclude target/ \
   --exclude output/ \
+  --exclude logs/ \
   --exclude src/main/java/brbo/fuzz/drivers/ \
   --recursive $HOME/Documents/workspace/brbo2-impl/ root@134.209.201.43:/root/Documents/workspace/brbo2-impl/
 
@@ -11,5 +12,6 @@ rsync --archive --compress --verbose --progress --executability --delete \
   --human-readable \
   --exclude target/ \
   --exclude output/ \
+  --exclude logs/ \
   --exclude src/main/java/brbo/fuzz/drivers/ \
   --recursive $HOME/Documents/workspace/brbo2-impl/ root@64.227.67.224:/root/Documents/workspace/brbo2-impl/
