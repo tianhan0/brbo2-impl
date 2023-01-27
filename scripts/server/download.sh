0776605ae31200018b5d7a437cd2dc5dc062d57a
Tianhan <tianhanlu@gmail.com>
2023-01-27 13:58:40 -0800

    Update download.sh

--- scripts/server/download.sh
+++ scripts/server/download.sh
@@ -6,6 +6,9 @@ rsync --archive --compress --verbose --progress --executability \
   --exclude output/ \
   --exclude src/main/java/brbo/fuzz/drivers/ \
   --exclude .git/ \
+  --exclude src/ \
+  --exclude lib/ \
+  --exclude scripts/ \
   --recursive root@134.209.201.43:/root/Documents/workspace/brbo2-impl/ $HOME/Documents/workspace/brbo2-impl/
 
 rsync --archive --compress --verbose --progress --executability \
@@ -14,4 +17,7 @@ rsync --archive --compress --verbose --progress --executability \
   --exclude output/ \
   --exclude src/main/java/brbo/fuzz/drivers/ \
   --exclude .git/ \
+  --exclude src/ \
+  --exclude lib/ \
+  --exclude scripts/ \
   --recursive root@64.227.67.224:/root/Documents/workspace/brbo2-impl/ $HOME/Documents/workspace/brbo2-impl/
