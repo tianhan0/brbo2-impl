f5ced1ce8bdcedaca5c3e8b80f5ac84a03f93cd0
Tianhan <tianhanlu@gmail.com>
2023-01-27 08:27:08 -0800

    Backup the stac/ examples

---
+++ src/main/java/brbo/benchmarks/archive_sas22/stac/TemplateEngine.java
@@ -0,0 +1,28 @@
+package brbo.benchmarks.archive_sas22.stac;
+
+import brbo.benchmarks.Common;
+
+public abstract class TemplateEngine extends Common {
+  void execute(int[] text) {
+    if (arrayLength(text) <= 1) {
+      return;
+    }
+    int R = 0;
+    mostPreciseBound(R <= arraySum(text));
+    lessPreciseBound(R <= MAX * arraySum(text) + MAX);
+
+    int chunk = 0;
+    int tag = 0;
+    int i = 0;
+    chunk = arrayRead(text, i);
+    R = R + chunk;
+    i++;
+    while (i + 1 < arrayLength(text)) {
+      tag = arrayRead(text, i);
+      i++;
+      chunk = arrayRead(text, i);
+      R = R + chunk;
+      i++;
+    }
+  }
+}
---
+++ src/main/java/brbo/benchmarks/archive_sas22/stac/TemplateEngine2.java
@@ -0,0 +1,43 @@
+package brbo.benchmarks.archive_sas22.stac;
+
+import brbo.benchmarks.Common;
+
+public abstract class TemplateEngine2 extends Common {
+  void execute(int[] text, int templateds, int separator) {
+    if (arrayLength(text) <= 1 || templateds <= 0 || separator <= 0) {
+      return;
+    }
+    int R = 0;
+    mostPreciseBound(R <= arraySum(text) * templateds + separator * templateds);
+    lessPreciseBound(R <= MAX * arraySum(text) * templateds +
+        MAX * separator * templateds +
+        MAX * separator * arraySum(text) +
+        MAX * arraySum(text) * arraySum(text) +
+        MAX * separator * separator +
+        MAX * templateds * templateds +
+        MAX * arraySum(text) +
+        MAX * templateds + MAX * separator +
+        MAX
+    );
+
+    int i = 0;
+    int j = 0;
+    int chunk = 0;
+    int tag = 0;
+    while (i < templateds) {
+      j = 0;
+      chunk = arrayRead(text, j);
+      R = R + chunk;
+      j++;
+      while (j + 1 < arrayLength(text)) {
+        tag = arrayRead(text, j);
+        j++;
+        chunk = arrayRead(text, j);
+        R = R + chunk;
+        j++;
+      }
+      i++;
+      R = R + separator;
+    }
+  }
+}
