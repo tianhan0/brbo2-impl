c426d198f121cd02d7050a4e2349b1960fba8587
Tianhan <tianhanlu@gmail.com>
2023-01-29 19:45:55 -0800

    Update PredefinedVariables.scala

--- src/main/scala/brbo/common/PredefinedVariables.scala
+++ src/main/scala/brbo/common/PredefinedVariables.scala
@@ -1,13 +1,14 @@
 package brbo.common
 
+import brbo.backend2.qfuzz.DriverGenerator
+
 object PredefinedVariables {
   val MAX = 8
   val LARGE_INT = 10000000
 
-  val variableNames: List[String] = List("MAX", "LARGE_INT").sorted
-
   val variables: Map[String, Int] = Map(
     "MAX" -> MAX,
     "LARGE_INT" -> LARGE_INT,
+    "BOOLEAN_SEPARATOR" -> DriverGenerator.BOOLEAN_SEPARATOR,
   )
 }
