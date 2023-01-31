2c48ca33af1174d11b00683b866f8017cd07cec1
Tianhan <tianhanlu@gmail.com>
2023-01-30 17:31:17 -0800

    Create TemplateEngineSimplified2.java

---
+++ src/main/java/brbo/benchmarks/sas22/stac/TemplateEngineSimplified2.java
@@ -0,0 +1,20 @@
+package brbo.benchmarks.sas22.stac;
+
+import brbo.benchmarks.Common;
+
+public abstract class TemplateEngineSimplified2 extends Common {
+  void execute(int[] text) {
+    if (arrayLength(text) <= 1) {
+      return;
+    }
+    int R = 0;
+    mostPreciseBound(R <= arraySum(text));
+    lessPreciseBound(R <= MAX * arraySum(text) + MAX);
+
+    int chunk = 0;
+    for (int i = 0; i < arrayLength(text); i++) {
+      chunk = arrayRead(text, i);
+      R = R + chunk;
+    }
+  }
+}
